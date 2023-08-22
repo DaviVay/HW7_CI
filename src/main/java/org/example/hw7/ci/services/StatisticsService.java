045b0b1d4de5be79ceedb3c8c854f7db10d4c8dd
Васенева <di_vaseneva@mail.ru>
2023-08-22 18:19:30 +0500

    CI

---
+++ src/main/java/org/example/hw7/ci/services/StatisticsService.java
@@ -0,0 +1,14 @@
+package org.example.hw7.ci.services;
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
+
+
